b891e60fe04c786a43535c8b2c350710e2ebb0e2
eduardo aleixo <eduardoaleixomartins@gmail.com>
2019-10-05 00:34:48 -0300

    decrease element's size to be able to fit the keyboard on the screen

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wanikani Self-Study Mobile Enhancements
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  improve mobile support for wanikani self-study
 // @author       eh-am
 // @match        *://*.wanikani.com/*
@@ -18,7 +18,42 @@
 		#ss_quiz {
 			max-width: 100vw;
 			left: 0 !important;
+			top: 0 !important;
 			box-sizing: border-box;
+		}
+
+		#ss_quiz .cfgbar {
+			display: flex;
+		}
+
+		#ss_quiz .atype {
+			font-size: 1.2em !important;
+		}
+
+
+		#ss_quiz .qwrap {
+			height: 6.em !important;
+		}
+
+
+		#ss_quiz .qwrap .prev {
+			line-height: initial;
+
+			/* vertical align */
+			position: absolute;
+			top: 50%;
+			left: 0;
+			transform: translate(0, -50%);
+		}
+
+		#ss_quiz .qwrap .next {
+			line-height: initial;
+
+			/* vertical align */
+			position: absolute;
+			top: 50%;
+			right: 0;
+			transform: translate(0, -50%);
 		}
 	}
   `);
